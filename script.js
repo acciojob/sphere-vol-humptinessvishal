13469b5edd6c0ea1eeadfe2f2ae2d73e6fc3b28c
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-08-23 12:58:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function volume_sphere() {
     //Write your code here
 	const form = document.getElementById("MyForm");
 	const volume = document.getElementById("volume");
-	const radius =form["radius"].value;
+	const radius =parseFloat(form["radius"].value);
 	if (isNaN(radius) || radius < 0) {
         volume.value = "Invalid radius";
     }
 	else {
         const calculate = (4/3) * Math.PI * Math.pow(radius, 3);
-        volume.value = calculate;
+        volume.value = calculate.toFixed(2);
     }
 	return false;
 } 
