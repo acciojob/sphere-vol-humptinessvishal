c732bb9e4ccd0b4e07cc7160d56405c21bd15a18
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-08-23 12:51:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
     //Write your code here
-  
+	const form = document.getElementById("MyForm");
+	const volume = document.getElementById("volume");
+	const radius =form["radius"].value;
+	if (isNaN(radius) || radius < 0) {
+        volume.value = "Invalid radius";
+    }
+	else {
+        const calculate = (4/3) * Math.PI * Math.pow(radius, 3);
+        volume.value = calculate;
+    }
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
